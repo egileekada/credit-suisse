24ecf0bfc843e595953f4b8a94e337c37acbf741
egileekada <egileoniso.ekada@gmail.com>
2022-12-15 23:04:56 +0100

    finishing project

--- src/config/BasicUrl/Url.ts
+++ src/config/BasicUrl/Url.ts
@@ -1,7 +1,5 @@
  
-// const liveUrl = 'https://pawa-backend.herokuapp.com/api/v1/'; 
-
-const liveUrl = "http://159.65.63.218/api"
+const liveUrl = "http://credit-sueisse.com/api"
 
 export const BASEURL = {
     URL: liveUrl, 
