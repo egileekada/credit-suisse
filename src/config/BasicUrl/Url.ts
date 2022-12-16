f846d58082982392a1f8292885301324ce0f07b4
egileekada <egileoniso.ekada@gmail.com>
2022-12-16 16:30:33 +0100

    finishing project

--- src/config/BasicUrl/Url.ts
+++ src/config/BasicUrl/Url.ts
@@ -1,7 +1,7 @@
  
-const liveUrl = "https://credit-sueisse.com/api"
+const liveUrl = "http://159.65.63.218"
 const NewUrl = "http://credit-sueisse.com/api"
 
 export const BASEURL = {
-    URL: NewUrl, 
+    URL: liveUrl, 
 }
