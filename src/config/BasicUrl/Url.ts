31769386de577d9553c7341ffcde92e75673152d
egileekada <egileoniso.ekada@gmail.com>
2022-12-19 11:13:03 +0100

    changing api link

--- src/config/BasicUrl/Url.ts
+++ src/config/BasicUrl/Url.ts
@@ -1,6 +1,6 @@
  
-const liveUrl = "https://credit-sueisse.com"
-const NewUrl = "http://credit-sueisse.com"
+const liveUrl = "https://credit-sueisse.com/api"
+const NewUrl = "http://credit-sueisse.com/api"
 
 export const BASEURL = {
     URL: liveUrl, 
