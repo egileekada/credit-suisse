069ebad696a7cca45e59e69ca0dc01d7fdf214e5
egileekada <egileoniso.ekada@gmail.com>
2025-07-19 21:15:41 +0100

    update on country

--- src/components/SelectLocation/index.tsx
+++ src/components/SelectLocation/index.tsx
@@ -1,22 +1,18 @@
-import React from 'react'
-import { Select } from '@chakra-ui/select';
+import { Select } from '@chakra-ui/react';
+// import React from 'react'
+import countries from 'i18n-iso-countries';
+import enLocale from 'i18n-iso-countries/langs/en.json';
 
-export default function Index(props: any) {
+export default function Index(props: any) { 
 
-    const [loadingOptions, setLoadingOptions] = React.useState(false)
-    const [locations, setLocations] = React.useState<Array<string>>([])
+    countries.registerLocale(enLocale);
 
-    React.useEffect(() => {
-        setLoadingOptions(true)
-        fetch('https://restcountries.com/v3.1/all')
-            .then(response => response.json())
-            .then(data => {
-                const countryNames = data.map((country: any) => country.name.common);
-                setLocations([...countryNames])
-                setLoadingOptions(false)
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    }, []);
+    const countryObj = countries.getNames('en', { select: 'official' });
+
+    const countryList = Object.entries(countryObj).map(([code,name]) => ({
+        label: name,
+        value: code,
+    }));
 
     const ChangeHandler = (event: any) => {
         if (props.name === "Nationality") {
@@ -24,22 +20,21 @@ export default function Index(props: any) {
         } else {
             props.birth(event.target.value)
         }
-    }
+    } 
 
+    
     return (
-        <div className=' w-full flex justify-center ' >
-            {loadingOptions && (
-                <p>Loading...</p>
-            )}
-            {!loadingOptions && (
+        <div className=' w-full flex justify-center ' > 
                 <Select onChange={(e) => ChangeHandler(e)} placeholder={props.name} fontSize="14px" height="50px" border="1px solid #E1E2E5" border-radius="5px"  >
-                    {locations.map((item: any, index: any) => {
+                    {countryList.map((item: {
+                        label: string,
+                        value: string,
+                    }, index: any) => {
                         return (
-                            <option key={index} >{item}</option>
+                            <option key={index} value={item.label} >{item.label}</option>
                         )
                     })}
-                </Select>
-            )}
+                </Select> 
         </div>
     )
 } 
